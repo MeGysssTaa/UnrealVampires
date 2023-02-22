a5812b22d1c5e1b5c3f70eee17dafd4a55b33ac3
German Vekhorev <darksidecode@ya.ru>
2023-02-22 18:46:48 +0200

    First commit

---
+++ Source/UnrealVampires/Private/TestCppActor1.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TestCppActor1.h"
+
+// Sets default values
+ATestCppActor1::ATestCppActor1()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ATestCppActor1::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("Hi from C++!!! 2"));
+}
+
+// Called every frame
+void ATestCppActor1::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	UE_LOG(LogTemp, Warning, TEXT("Hi from C++!!! 3"));
+}
+
---
+++ Source/UnrealVampires/UnrealVampires.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UnrealVampires.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealVampires, "UnrealVampires" );
